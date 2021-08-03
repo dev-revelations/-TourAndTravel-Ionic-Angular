961e6de8fbabc5792e70b82d05d43e3926067a0a
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-03 18:30:53 +0100

    Making responsive navbar

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const exports = [
   HeaderComponent,
-  LogoComponent
+  LogoComponent,
+  NavbarComponent
 ];
 
 @NgModule({
