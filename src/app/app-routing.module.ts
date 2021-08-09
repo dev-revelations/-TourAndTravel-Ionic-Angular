00b086d3fbdcf806d1af9500a39a1a9f0813c33e
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-09 14:54:29 +0100

    Fixing content scroll by anchor tag click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, anchorScrolling: 'enabled' })
   ],
   exports: [RouterModule]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,61 @@
-import { Component, HostListener } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { AfterViewInit, Component, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonContent, MenuController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit, OnDestroy {
 
   loginFormActive = false;
 
-  constructor(private menu: MenuController) { }
+  @ViewChild('content')
+  content: IonContent;
+
+  scrollElement: HTMLElement;
+
+  routeSubscription: Subscription;
+
+  constructor(private menu: MenuController, private route: ActivatedRoute) {
+    this.routeSubscription = route.fragment.subscribe(fragment => {
+      if (fragment && this, this.scrollElement) {
+        this.scrollToFragment(fragment);
+        this.menu.close();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.content.getScrollElement()
+      .then(element => {
+        element.style.setProperty('scroll-padding', '6rem')
+        this.scrollElement = element;
+      });
+  }
 
   @HostListener('window:resize')
   onWindowResize() {
     this.menu.close();
+  }
+
+  scroll(x, y) {
+    this.content.scrollToPoint(x, y, 1000);
+  }
+
+  scrollToFragment(fragment: string) {
+    const fragmentElement = document.getElementById(fragment);
+    const offsetTop = fragmentElement.offsetTop - 35;
+    this.scroll(0, offsetTop);
   }
 
 }
